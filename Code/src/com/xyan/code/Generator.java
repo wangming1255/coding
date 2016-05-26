c169916061c1d503f8fdc9004449bf9c4fe66c8f
wangming1255 <1255321142@qq.com>
2016-05-26 15:00:21 +0800

    添加注释

--- Code/src/com/xyan/code/Generator.java
+++ Code/src/com/xyan/code/Generator.java
@@ -3,6 +3,10 @@ package com.xyan.code;
 import com.xyan.code.service.CodeService;
 import com.xyan.code.service.impl.CodeServiceImpl;
 
+/**
+ *入口，运行此方法即可生成代码
+ * @author wangming
+ */
 public class Generator {
 	
 	public static void main(String[] args) throws Exception {
